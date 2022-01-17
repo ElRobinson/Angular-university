2e9bffbf16983b7013eed6870fc3d351e227755d
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2022-01-16 21:31:59 -0300

    learning some key features

--- angular-course-initial/src/app/app.component.ts
+++ angular-course-initial/src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular course';
+
+  onLogoClicked(){
+    alert("ok");
+  }
+
+  onKeyUp(newTitle:string){
+    this.title = newTitle;
+  }
 }
